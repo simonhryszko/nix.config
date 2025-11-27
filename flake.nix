be4ceb14600625d73fbeebf04116f2627e423a63
Simon Hryszko <simonhryszko@gmail.com>
2025-11-27 21:12:26 +0700

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of simon";
+  description = "Home Manager and NixOS configuration of simon";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
@@ -17,14 +17,28 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      # NixOS configuration
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.simon = import ./home.nix;
+          }
+        ];
+      };
+
+      # Standalone Home Manager configuration
       homeConfigurations.simon = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
-		# ./configuration.nix
-		./home.nix 
+		./home.nix
 	];
 
         # Optionally use extraSpecialArgs
